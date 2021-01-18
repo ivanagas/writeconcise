fdb4e5c5729482901f49e603a42e89dbc3e2a83c
ivanagas <34755028+ivanagas@users.noreply.github.com>
2021-01-17 21:20:50 -0800

    modal, help instructions, email me, spacing

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="pt-3 pb-4 pl-4">
       <h3>Shorter Letter</h3> 
-      <h5>Write With Better Words // <span>Help</span></h5>
+      <h5 class="text-muted">Write With Better Words // <span class="modal-btn" v-b-modal.modal-1>What Is This?</span></h5>
     </div>
     <div class="row justify-content-between">
       <div class="col-7">
@@ -25,8 +25,15 @@
         <words ref="words" @keywordChange="replaceContent"></words>
       </div>
     </div>
+    <div>
+      <b-modal id="modal-1" title="What is Shorter Letter?" :ok-only="true">
+        <p>Shorter Letter is a tool to help you write better.</p>
+        <p>Enter words you don't want to use under <b>Highlight</b> and they are highlighted in the main text. Edit them to use better words, create better sentences, and make your writing better.</p>
+        <p>Any feedback or issues? <a href="mailto:ianvanagas@gmail.com" target="_blank" rel="noopener">Send me an email.</a></p>
+      </b-modal>
+    </div>
     <div class="footer fixed-bottom p-2">
-        <h5 class="float-right"><a href="https://twitter.com/IanVanagas" class="text-muted">Made by Ian Vanagas</a></h5>
+        <h5 class="float-right"><a href="https://twitter.com/IanVanagas" target="_blank" rel="noopener" class="text-muted">Made by Ian Vanagas</a></h5>
     </div>
   </div>
 </template>
@@ -37,14 +44,6 @@ import Words from './Words.vue'
 export default {
   components: {
     Words
-  },
-  data() {
-    return {
-      input: '',
-      output: '',
-      swag: '',
-      yoshi: '',
-    }
   },
   mounted() {
     const target = document.getElementById('good-input');
@@ -101,7 +100,6 @@ export default {
 
       if (this.$refs.words.keywordList.length) {
         var joinedKeywords = this.$refs.words.keywordList.join('|')
-        console.log(joinedKeywords)
         var keywordRegex = new RegExp(joinedKeywords, 'gi')
       }
 
@@ -130,7 +128,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
   .hashtag-textarea {
     position: relative;
     overflow: auto;
@@ -157,7 +154,8 @@ export default {
     width: 100%;
     white-space: pre-wrap;
     overflow-wrap: break-word;
-  }    
+  }
+
   .overlay {
     -webkit-tap-highlight-color: transparent;
     padding: 0 23px;
@@ -167,10 +165,20 @@ export default {
     white-space: pre-wrap;
     overflow-wrap: break-word; 
   }
+
   [contenteditable][placeholder]:empty:before {
     content: attr(placeholder);
     position: absolute; 
     color: gray;
     background-color: transparent;
+  }
+
+  .modal-btn:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+
+  .modal-btn:focus {
+     outline: 0;
   }
 </style>
