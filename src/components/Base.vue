16d3cf59ad47b223bfce5e06eee11b91b9d4d7be
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2022-07-29 14:29:48 -0700

    modal component
    modal component

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -40,31 +40,7 @@
       </div>
     </div>
     <div class="row"><br><br></div>
-    <div>
-      <b-modal id="modal-1" title="What is Write Concise?" hide-footer>
-        <p>Write Concise is a tool to help you write better by writing less based on my editing workflow.</p> 
-        <p>Wondering why and how to write concisely? Read these:</p>
-        <ul>
-          <li>
-            <a href="https://www.orwell.ru/library/essays/politics/english/e_polit/" class="text-info" target="_blank" rel="noopener">
-              Politics and the English Language by George Orwell
-            </a>
-          </li>
-          <li>
-            <a href="https://dilbertblog.typepad.com/the_dilbert_blog/2007/06/the_day_you_bec.html" class="text-info" target="_blank" rel="noopener">
-              The Day You Became A Better Writer by Scott Adams
-            </a>
-          </li>
-          <li>
-            <a href="http://paulgraham.com/writing44.html" class="text-info" target="_blank" rel="noopener">
-              Writing, Briefly by Paul Graham
-            </a>
-          </li>
-        </ul>
-        <p>Write Concise is made by <a href="https://ianvanagas.com/" target="_blank" rel="noopener" class="text-info">Ian Vanagas</a></p>
-        <p>Any feedback or issues? <a href="mailto:ianvanagas@gmail.com" class="text-info" target="_blank" rel="noopener">Send me an email.</a></p>
-      </b-modal>
-    </div>
+    <modal />
   </div>
 </template>
 
@@ -72,12 +48,14 @@
 import Uncommon from './Uncommon.vue';
 import WordCount from './WordCount.vue';
 import Highlight from './Highlight.vue';
+import Modal from './Modal.vue';
 
 export default {
   components: {
     Highlight,
     Uncommon,
-    WordCount
+    WordCount,
+    Modal
   },
   data () {
     return {
