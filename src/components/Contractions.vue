9e5b34ab50f0c9eb6bea9e1406ea103acfc2cf12
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2022-08-05 19:10:05 -0700

    typo

--- src/components/Contractions.vue
+++ src/components/Contractions.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="row">
-      <h4 v-b-toggle.contraction class="contraction">Contractions</h4>
+      <h4 v-b-toggle.contraction class="contraction">
+        Contractions
+      </h4>
     </div>
     <b-collapse id="contraction">
       <div class="row">
@@ -12,7 +14,9 @@
             id="switch" 
             @change="flipSwitch()"
           >
-          <label class="custom-control-label" for="switch">Highlight</label>
+          <label class="custom-control-label" for="switch">
+            Highlight
+          </label>
         </div>
       </div>
     </b-collapse>
@@ -39,7 +43,7 @@ export default {
   .contraction:hover {
     text-decoration: underline;
   }
-  .custom-control-input:checked ~ .custom-control-label::before{ 
+  .custom-control-input:checked ~ .custom-control-label::before { 
     border-color: #ffc107 !important;
     background-color: #ffc107 !important;
   }
