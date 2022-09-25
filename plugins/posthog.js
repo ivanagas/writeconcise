f7e68eacad48a22b7ecacc70bc3d65cac7185aba
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2022-09-25 13:10:35 -0700

    format

--- plugins/posthog.js
+++ plugins/posthog.js
@@ -2,13 +2,13 @@ import posthog from "posthog-js";
 
 export default {
   install(Vue) {
-		if (!window.location.href.includes('localhost:8080')) {
-			Vue.prototype.$posthog = posthog.init(
-				'phc_QlwgYexW2lCA7CGMbqLoaP9kcFogYgUo4WEC0gWLbyt',
-				{
-					api_host: "https://app.posthog.com"
-				}
-			);
-		}
+    if (!window.location.href.includes('localhost:8080')) {
+      Vue.prototype.$posthog = posthog.init(
+        'phc_QlwgYexW2lCA7CGMbqLoaP9kcFogYgUo4WEC0gWLbyt',
+        {
+          api_host: "https://app.posthog.com"
+        }
+      );
+    }
   }
 };
