f1867ca62fa5f2a03eaf2dfbd9dd7f2853058465
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2024-02-06 08:59:26 -0800

    Update PostHog key

--- plugins/posthog.js
+++ plugins/posthog.js
@@ -4,7 +4,7 @@ export default {
   install(Vue) {
     if (!window.location.href.includes('localhost:8080')) {
       Vue.prototype.$posthog = posthog.init(
-        'phc_QlwgYexW2lCA7CGMbqLoaP9kcFogYgUo4WEC0gWLbyt',
+        'phc_BSvL6de4HPva0n9vVCSHQQyPpusblaA3JGpn45wy9h9',
         {
           api_host: "https://app.posthog.com"
         }
