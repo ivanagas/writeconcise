d123e2b9e934a4d5abc391e9f5766ae8e4728878
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2024-07-04 14:15:50 -0700

    mixpanel!

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import posthogPlugin from '../plugins/posthog'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import mixpanel from 'mixpanel-browser';
+ 
+mixpanel.init('31b097940e889b5d09a63961d808645c', {debug: true, track_pageview: true, persistence: 'localStorage'});
+
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
 Vue.use(posthogPlugin);
