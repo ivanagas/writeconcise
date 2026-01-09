956167d0af39670b76a6d3d9d83537ba7f969f14
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2026-01-09 18:47:16 -0300

    Updating packages (#20)
    * update some, qs
    
    * update more packages, fix posthog, etc

--- plugins/posthog.js
+++ plugins/posthog.js
@@ -2,13 +2,12 @@ import posthog from "posthog-js";
 
 export default {
   install(Vue) {
-    if (!window.location.href.includes('localhost:8080')) {
-      Vue.prototype.$posthog = posthog.init(
-        'phc_BSvL6de4HPva0n9vVCSHQQyPpusblaA3JGpn45wy9h9',
-        {
-          api_host: "https://app.posthog.com"
-        }
-      );
-    }
+    posthog.init(
+      'phc_BSvL6de4HPva0n9vVCSHQQyPpusblaA3JGpn45wy9h9',
+      {
+        api_host: "https://app.posthog.com"
+      }
+    );
+    Vue.prototype.$posthog = posthog;
   }
 };
